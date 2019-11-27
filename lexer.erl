84289f3dab6eb783c3e229e64b98367be41be5d7
David L. Inman <davidlevere@gmail.com>
2019-11-27 00:34:23 -0500

    lexer progress

--- lexer.erl
+++ lexer.erl
@@ -2,5 +2,29 @@
 -module(lexer).
 -export([lex/1]).
 
+% white space 
+lex([$ |Rest], Tokens) -> lex(Rest, Tokens);
+lex([$\r|Rest], Tokens) -> lex(Rest, Tokens);
+lex([$\n|Rest], Tokens) -> lex(Rest, Tokens);
+lex([$\t|Rest], Tokens) -> lex(Rest, Tokens);
+
+% brackets
+lex([${|Rest], Tokens) -> lex(Rest, [lcurl|Tokens]);
+lex([$}|Rest], Tokens) -> lex(Rest, [rcurl|Tokens]);
+lex([$(|Rest], Tokens) -> lex(Rest, [lparen|Tokens]);
+lex([$)|Rest], Tokens) -> lex(Rest, [rparen|Tokens]);
+
+% etc
+lex([$=|Rest], Tokens) -> lex(Rest, [equal|Tokens]);
+lex([$-,$>|Rest], Tokens) -> lex(Rest, [rarrow|Tokens]);
+
+% unknown token
+lex([Value|Rest], _) -> {fail, Value, length( Rest )}.
+
 
 lex(Input) -> 
+    case lex(Input, []) of
+        {ok, Tokens} -> {ok, lists:reverse( Tokens )};
+        Result -> Result
+    end .
+
