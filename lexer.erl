3cb7713732ac0ddde62c1e5367bc87b10e42d79f
David L. Inman <davidlevere@gmail.com>
2019-11-28 19:23:43 -0500

    lex int

--- lexer.erl
+++ lexer.erl
@@ -2,16 +2,20 @@
 -module(lexer).
 -export([lex/1]).
 
--define(is_start_symbol(S), (is_integer(S)
-                            and (((S >= $a) and (S =< $z))
-                              or ((S >= $A) and (S =< $Z))
-                              or (S == $_)))).
+-define(is_start_symbol(S), (((S >= $a) and (S =< $z))
+                          or ((S >= $A) and (S =< $Z))
+                           or (S == $_))).
 
--define(is_symbol(S), (is_integer(S)
-                      and (((S >= $a) and (S =< $z))
-                        or ((S >= $A) and (S =< $Z))
-                        or ((S >= $0) and (S =< $9))
-                        or (S == $_)))).
+-define(is_symbol(S), (((S >= $a) and (S =< $z))
+                    or ((S >= $A) and (S =< $Z))
+                    or ((S >= $0) and (S =< $9))
+                     or (S == $_))).
+
+-define(is_start_int(N), (((N >= $0) and (N =< $9))
+                        or (N == $-))).
+
+-define(is_int(N), ((N >= $0) and (N =< $9))).
+                          
 
 % symbol
 lex_symbol([], Chars, Tokens) -> 
@@ -27,6 +31,14 @@ lex_string([$"|Rest], Chars, Tokens) ->
     lex(Rest, [{string, lists:reverse(Chars)}|Tokens]);
 lex_string([V|Rest], Chars, Tokens) ->
     lex_string(Rest, [V|Chars], Tokens).
+
+% int
+lex_int([], Chars, Tokens) -> 
+    lex([], [{int, lists:reverse(Chars)}|Tokens]);
+lex_int([N|Rest], Chars, Tokens) when not ?is_int(N) -> 
+    lex([N|Rest], [{int, lists:reverse(Chars)}|Tokens]);
+lex_int([N|Rest], Chars, Tokens) when ?is_int(N) -> 
+    lex_symbol(Rest, [N|Chars], Tokens).
 
 % white space 
 lex([$ |Rest], Tokens) -> lex(Rest, Tokens);
@@ -49,6 +61,9 @@ lex([S|Rest], Tokens) when ?is_start_symbol(S) -> lex_symbol(Rest, [S], Tokens);
 
 % string
 lex([$"|Rest], Tokens) -> lex_string(Rest, [], Tokens);
+
+% int
+lex([N|Rest], Tokens) when ?is_start_int(N) -> lex_int(Rest, [N], Tokens);
 
 % end of file
 lex([], Tokens) -> {ok, Tokens};
