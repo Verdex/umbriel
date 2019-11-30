d9cbce984ef1e5db2450b83c7b12b9d21dc250bc
David L. Inman <davidlevere@gmail.com>
2019-11-30 11:08:22 -0500

    setting up module parser and module item parser

--- parser.erl
+++ parser.erl
@@ -2,7 +2,32 @@
 -module(parser).
 -export([parse/1]).
 
+% parse let
+parse_module_items([{symbol, "let"}, {symbol, ValName}|Rest], Items) -> fail;
+
+% parse open
+parse_module_items([{symbol, "open"}, {symbol, OpenName}, semicolon|Rest], Items) ->
+    parse_module_items(Rest, [{open_def, OpenName}|Items]);
+    
+% end of module definition
+parse_module_items([rcurl|Rest], Items) -> {ok, Items, Rest}.
 
 
+% parse modules
+parse_modules([{symbol, "mod"}, {symbol, ModName}, lcurl|Rest], Modules) -> 
+    case parse_module_items(Rest, []) of
+        {ok, Items, ItemRest} -> parse_modules(ItemRest, [{module, Items}|Modules]),
+        Else -> Else
+    end .
 
-parse(Tokens) -> 4.
+% end of tokens
+parse_modules([], Modules) -> {ok, Modules};
+
+% unknown token
+parse_modules(_, _) -> fail.
+
+parse(Tokens) ->
+    case parse_modules(Tokens, []) of
+        {ok, Modules} -> { ok, Modules },
+        Else -> Else
+    end .
